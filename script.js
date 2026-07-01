49d5dec2c5aabd1117961d8c6ab29efd715754af
sidkid1128-create <sidkid1128@gmail.com>
2026-07-01 04:42:16 -0400

    Prevent default action for anchor links with href='#'

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.querySelectorAll("a").forEach(link=>{
+    link.addEventListener("click",e=>{
+        if(link.getAttribute("href")==="#"){
+            e.preventDefault();
+        }
+    });
+});
 
+console.log("Storms Collections Loaded");
